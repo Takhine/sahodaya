f355b7ed2d1e4973a897d00e3013e6a146258dec
Aniketh <anikethnair97@gmail.com>
2019-10-18 10:52:03 +0530

    Navbar Layout complete

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 
+// MATERIAL
+import {
+    AppBar,
+    Toolbar,
+    IconButton,
+    MenuItem,
+    Menu,
+    Button,
+    Drawer,
+    Grid,
+    Tabs,
+    Tab,
+    Typography,
+  } from '@material-ui/core';
+  import MenuIcon from '@material-ui/icons/Menu';
 
+  import logo from 'static/images/cbse-logo.png';
 function Navbar() {
     return (
-        <React.Fragment>
-            Navbar
-        </React.Fragment>
+        <div className="grow">
+            <AppBar position="fixed" id="navbar">
+                <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="Open drawer"
+                    edge="start"
+                    // onClick={toggleDrawer('left', true)}
+                    className="menu-button"
+                >
+                    <MenuIcon />
+                </IconButton>
+                <h2>Sahodaya School Complex MMR OMMR</h2>
+                <div className="grow"/>
+                <div className="logo-container">
+                    <img src={logo} alt="Sahodaya School Complex Logo" />
+                </div>
+                <div className="nav-menu-container">
+                <Button color="inherit">Home</Button>
+                <Button color="inherit">About</Button>
+                <Button color="inherit">Events</Button>
+                <Button color="inherit">Archives</Button>
+                </div>
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
 
